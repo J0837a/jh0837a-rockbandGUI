4ee5ef7c2cf8d16c7ed85ed92fd7b195064f556c
marin <jh0837a@student.american.edu>
2022-09-02 15:52:55 -0700

    We can download files now, very pog indeed :)

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
 import requests as API
+import webbrowser as web
+import pygame as pyg
+#import math as meh
+
+yt_vid = "https://www.youtube.com/watch?v=Oo8H5vUKMlk"
+
+params = {
+    "url": yt_vid,
+    "format": 'mp3',
+}
+
+headers = {
+        'X-RapidAPI-Key': '09ff266df6msh583568c18c6ed0bp116179jsn2292c3aa22f6',
+        'X-RapidAPI-Host': 't-one-youtube-converter.p.rapidapi.com'
+}
+
+raw_video_obj = API.get("https://t-one-youtube-converter.p.rapidapi.com/api/v1/createProcess", headers=headers,  params=params)
+converted_video_obj = raw_video_obj.json()
+web.open(converted_video_obj["file"])
+
+#for x in range(1, 100):
+#   if meh.ceil(x / 2) == x / 2:
+#        print(x)
